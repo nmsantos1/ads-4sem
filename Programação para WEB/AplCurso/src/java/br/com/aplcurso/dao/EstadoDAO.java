c9302c790379bb8e1514380a3f7eb37b6b1e594e
Natanael Marconato dos Santos <nmsantos1@users.noreply.github.com>
2023-03-13 22:16:36 -0300

    AplCurso - Figura 92

--- Programação para WEB/AplCurso/src/java/br/com/aplcurso/dao/EstadoDAO.java
+++ Programação para WEB/AplCurso/src/java/br/com/aplcurso/dao/EstadoDAO.java
@@ -19,27 +19,115 @@ public class EstadoDAO implements GenericDAO {
     
     @Override
     public Boolean cadastrar(Object objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Estado oEstado = (Estado) objeto;
+	Boolean retorno = false;
+        if (oEstado.getIdEstado() == 0) {
+		retorno = this.inserir(oEstado);
+	} else {
+		retorno = this.alterar(oEstado);
+	}
+	return retorno;
     }
 
     @Override
     public Boolean inserir(Object objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Estado oEstado = (Estado) objeto;
+	PreparedStatement stmt = null;
+	String sql = "insert into estado (nomeestado,siglaestado) values (?,?)";
+	try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, oEstado.getNomeEstado());
+            stmt.setString(2, oEstado.getSiglaEstado());
+            stmt.execute();
+            conexao.commit();
+            return true;
+	} catch (Exception ex) {
+            try {
+		System.out.println("Problema ao cadastrar o Estado! Erro: "+ex.getMessage());
+		ex.printStackTrace();
+		conexao.rollback();
+            } catch (SQLException e) {
+		System.out.println("Erro: "+e.getMessage());
+		e.printStackTrace();
+            }
+	}
+	return false;
     }
 
     @Override
     public Boolean alterar(Object objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Estado oEstado = (Estado) objeto;
+        PreparedStatement stmt = null;
+        String sql = "update estado set nomeestado=?,siglaestado=? where idestado=?";
+        try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, oEstado.getNomeEstado());
+            stmt.setString(2, oEstado.getSiglaEstado());
+            stmt.setInt(3, oEstado.getIdEstado());
+            stmt.execute();
+            conexao.commit();
+            return true;
+        } catch (Exception ex) {
+            try {
+                System.out.println("Problemas ao alterar o Estado! Erro: "+ex.getMessage());
+                ex.printStackTrace();
+                conexao.rollback();
+            } catch (SQLException e) {
+                System.out.println("Erro:"+e.getMessage());
+                e.printStackTrace();
+            }
+            return false;
+        }
     }
 
     @Override
     public Boolean excluir(int numero) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int idEstado = numero;
+        PreparedStatement stmt = null;
+        
+        String sql = "delete from estado where idestado=?";
+        try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, idEstado);
+            stmt.execute();
+            conexao.commit();
+            return true;
+        } catch (Exception ex) {
+            System.out.println("Problemas ao excluir a Estado! Erro: "
+                + ex.getMessage());
+            try {
+                conexao.rollback();
+            } catch (SQLException e) {
+                System.out.println("Erro rollback "+e.getMessage());
+                e.printStackTrace();
+            }
+            return false;
+        }
     }
 
     @Override
     public Object carregar(int numero) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int idEstado = numero;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Estado oEstado = null;
+        String sql="select * from estado where idEstado=?";
+        
+        try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, idEstado);
+            rs=stmt.executeQuery();
+            while (rs.next()) {
+                oEstado = new Estado();
+                oEstado.setIdEstado(rs.getInt("idEstado"));
+                oEstado.setNomeEstado(rs.getString("nomeestado"));
+                oEstado.setSiglaEstado(rs.getString("siglaestado"));
+            }
+            return oEstado;
+        } catch (Exception ex) {
+            System.out.println("Problemas ao carregar Estado! Erro:"+ex.getMessage());
+            return false;
+        }
     }
 
     @Override
